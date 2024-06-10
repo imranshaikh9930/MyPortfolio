c88a22c65c64296fdd2bde3fc0859d8cf76be640
imran <imranpathan7040@gmail.com>
2024-06-11 00:08:09 +0530

    code update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen   bg-[#0a192f]'>
       {/* Container */}
-      
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         
         <p className='text-pink-600'>Hi, my name is</p>
@@ -44,7 +43,8 @@ const Home = () => {
             </Link>
       
         </div>
-        
+       >
+      
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   
 
 
-
+6
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-[100]">
       <div>
@@ -67,7 +67,7 @@ const Navbar = () => {
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#fff] dark:bg-[#0a192f] flex flex-col justify-center items-center"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
         <li className="py-6 text-2xl ">
